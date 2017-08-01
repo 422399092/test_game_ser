80ed44fa6635632f8e2fce8acf36a766a05fef62
liyuanxing <liyuanxing@yy.com>
2017-08-01 10:49:29 +0800

    up code

--- src/db/DBServer.lua
+++ src/db/DBServer.lua
@@ -3,6 +3,6 @@ local skynet = require "skynet"
 local dbHandler
 
 skynet.start(function()
-	dbHandler = skynet.uniqueservice("DBHandler")
+  dbHandler = skynet.uniqueservice("DBHandler")
   skynet.exit()
 end)
